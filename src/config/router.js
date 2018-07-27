3ccff4e333b5fb83af329a12f95af9250dd608ad
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-07-26 22:20:11 -0300

    Mapeamento de rotas

---
+++ src/config/router.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+module.exports = function (server) {
+
+    //URL base
+    const router = express.Router()
+    server.use('/api', router)
+
+    //Rotas do ciclo de pagamento
+
+    const BillingCycle = require('../api/billingCycle/billingCycleService')
+    BillingCycle.register(router, '/billingCycles')
+}

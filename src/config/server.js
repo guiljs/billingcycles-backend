4e3eb1e3d3933e2b19a6dd3d9404caea8ebedd71
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-07-28 16:59:08 -0300

    Inclui o express-query-int para querystring aceitar int

--- src/config/server.js
+++ src/config/server.js
@@ -3,11 +3,13 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const cors = require('./cors')
+const expressInt = require('express-query-int')
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(cors)
+server.use(expressInt())
 
 server.listen(port, function(){
     console.log(`BACKEND está rodando na porta ${port}`)
